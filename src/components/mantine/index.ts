2cdea20c532f1f553e5f63da67b656816bd19f30
Jeffrey Bernadas <jeffrey.bernadas0@gmail.com>
2025-03-10 23:55:06 +0800

    Feat: Add Mantine ThemeProvider and index for components

---
+++ src/components/mantine/index.ts
@@ -0,0 +1,6 @@
+// Re-export all Mantine components here
+// Example:
+// export { Button as MantineButton } from "./Button";
+// export { Card as MantineCard } from "./Card";
+
+// This is a placeholder. Add your Mantine component exports as you create them
