e6c65d09158e255082b9fea0a11466b71a8a941f
Jeffrey Bernadas <jeffrey.bernadas0@gmail.com>
2025-03-10 23:50:23 +0800

    Feat: Upgrade dependencies and support React 19 and TailwindCSS 4

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
 const autoprefixer = require("autoprefixer");
-const tailwindcss = require("tailwindcss");
+const tailwindPostcss = require("@tailwindcss/postcss");
 
 module.exports = {
-  plugins: [tailwindcss, autoprefixer],
+  plugins: [
+    require("postcss-preset-mantine")(),
+    require("postcss-simple-vars")({
+      variables: {
+        "mantine-breakpoint-xs": "36em",
+        "mantine-breakpoint-sm": "48em",
+        "mantine-breakpoint-md": "62em",
+        "mantine-breakpoint-lg": "75em",
+        "mantine-breakpoint-xl": "88em",
+      },
+    }),
+    autoprefixer,
+    tailwindPostcss,
+  ],
 };
